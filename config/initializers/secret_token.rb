d5b2225320b5a48dcd026e214a1363a9aa9f2879
Roman Tymoshuk <Roman.Tymoshuk@softwareag.com>
2012-07-09 19:20:16 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demog::Application.config.secret_token = '80778fd192912872f3c7ab4912b235bef8cbaeaacda676bd4a364a02bad36254b21aa7e82954ade093c81837f990b057aa6c1682aa2493b3d4493308ce59d2d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Demog::Application.config.session_store :cookie_store, key: '_demog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Demog::Application.config.session_store :active_record_store
